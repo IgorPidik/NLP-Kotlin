7e80e6e2233c9a120cf8355a16c8f8d147936740
igor <igorpidik@gmail.com>
2017-08-08 00:23:06 +0200

    minor file update

--- src/HMM_Tagger/main.kt
+++ src/HMM_Tagger/main.kt
@@ -4,6 +4,6 @@
 
 fun main(args: Array<String>) {
     val hmm = HMM_Tagger()
-    hmm.initCounts("gene.counts")
-    hmm.tagFile("gene.dev", "gene_dev_test.out")
+    hmm.initCounts("/mnt/Data/Projects/Kotlin-Projects/NLP/src/HMM_Tagger/Data/gene.counts")
+    hmm.tagFile("/mnt/Data/Projects/Kotlin-Projects/NLP/src/HMM_Tagger/Data/gene.dev", "/mnt/Data/Projects/Kotlin-Projects/NLP/src/HMM_Tagger/Data/gene_dev_test.out")
 }
